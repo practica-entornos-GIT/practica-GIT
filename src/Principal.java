eb68367c3bc46f3e5dc543234ba27f70ecc6c206
migguzp <150238930+migguzp@users.noreply.github.com>
2024-02-23 00:21:24 +0100

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,7 @@ public class Principal {
 	public static void main(String[] args) {
 		Coche coche = new Coche("1111AAA",10000);
 		System.out.println(coche);
+		System.out.println("Este es el modelo del coche:")
 	}
 }
 //Comentario para realizar el segundo commit
