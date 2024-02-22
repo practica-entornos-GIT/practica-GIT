f42693133fa62be86081ce0ea0de8b80a9b6f1fd
migguzp <150238930+migguzp@users.noreply.github.com>
2024-02-23 00:56:54 +0100

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,7 @@ public class Principal {
 	public static void main(String[] args) {
 		Coche coche = new Coche("1111AAA",10000);
 		System.out.println(coche);
+		System.out.println(coche.matricula);
 	}
 }
 //Comentario para realizar el segundo commit
